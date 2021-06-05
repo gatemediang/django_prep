09d488dc27be4497c8f5b3711a253e700db06987
Tunji Ologun <estolog@gmail.com>
2021-06-05 09:50:00 +0100

    pges commit

--- kofi_app/views.py
+++ kofi_app/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+# def homePageView(request):
+#   return HttpResponse('Hello, World!')
+
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
